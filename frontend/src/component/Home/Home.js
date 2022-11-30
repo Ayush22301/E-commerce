adad3465ca51eca0dba3639bd44bb15d53957bc0
Ayush22301 <ayushsaxena.phy18@iitbhu.ac.in>
2022-11-30 21:32:05 +0530

    files added

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,25 +1,25 @@
-import React, { Fragment, useEffect } from "react";
-import { CgMouse } from "react-icons/all";
-import "./Home.css";
-import ProductCard from "./ProductCard.js";
-import MetaData from "../layout/MetaData";
-import { clearErrors, getProduct } from "../../actions/productAction";
-import { useSelector, useDispatch } from "react-redux";
-import Loader from "../layout/Loader/Loader";
-import { useAlert } from "react-alert";
+import React, { Fragment, useEffect } from 'react'
+import { CgMouse } from 'react-icons/all'
+import './Home.css'
+import ProductCard from './ProductCard.js'
+import MetaData from '../layout/MetaData'
+import { clearErrors, getProduct } from '../../actions/productAction'
+import { useSelector, useDispatch } from 'react-redux'
+import Loader from '../layout/Loader/Loader'
+import { useAlert } from 'react-alert'
 
 const Home = () => {
-  const alert = useAlert();
-  const dispatch = useDispatch();
-  const { loading, error, products } = useSelector((state) => state.products);
+  const alert = useAlert()
+  const dispatch = useDispatch()
+  const { loading, error, products } = useSelector((state) => state.products)
 
   useEffect(() => {
     if (error) {
-      alert.error(error);
-      dispatch(clearErrors());
+      alert.error(error)
+      dispatch(clearErrors())
     }
-    dispatch(getProduct());
-  }, [dispatch, error, alert]);
+    dispatch(getProduct())
+  }, [dispatch, error, alert])
 
   return (
     <Fragment>
@@ -27,22 +27,22 @@ const Home = () => {
         <Loader />
       ) : (
         <Fragment>
-          <MetaData title="ECOMMERCE" />
+          <MetaData title='ECOMMERCE' />
 
-          <div className="banner">
+          <div className='banner'>
             <p>Welcome to Ecommerce</p>
             <h1>FIND AMAZING PRODUCTS BELOW</h1>
 
-            <a href="#container">
+            <a href='#container'>
               <button>
                 Scroll <CgMouse />
               </button>
             </a>
           </div>
 
-          <h2 className="homeHeading">Featured Products</h2>
+          <h2 className='homeHeading'>Featured Products</h2>
 
-          <div className="container" id="container">
+          <div className='container' id='container'>
             {products &&
               products.map((product) => (
                 <ProductCard key={product._id} product={product} />
@@ -51,7 +51,7 @@ const Home = () => {
         </Fragment>
       )}
     </Fragment>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -1,35 +1,35 @@
-import React, { Fragment, useEffect, useState } from "react";
-import "./Products.css";
-import { useSelector, useDispatch } from "react-redux";
-import { clearErrors, getProduct } from "../../actions/productAction";
-import Loader from "../layout/Loader/Loader";
-import ProductCard from "../Home/ProductCard";
-import Pagination from "react-js-pagination";
-import Slider from "@material-ui/core/Slider";
-import { useAlert } from "react-alert";
-import Typography from "@material-ui/core/Typography";
-import MetaData from "../layout/MetaData";
+import React, { Fragment, useEffect, useState } from 'react'
+import './Products.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { clearErrors, getProduct } from '../../actions/productAction'
+import Loader from '../layout/Loader/Loader'
+import ProductCard from '../Home/ProductCard'
+import Pagination from 'react-js-pagination'
+import Slider from '@material-ui/core/Slider'
+import { useAlert } from 'react-alert'
+import Typography from '@material-ui/core/Typography'
+import MetaData from '../layout/MetaData'
 
 const categories = [
-  "Laptop",
-  "Footwear",
-  "Bottom",
-  "Tops",
-  "Attire",
-  "Camera",
-  "SmartPhones",
-];
+  'Laptop',
+  'Footwear',
+  'Bottom',
+  'Tops',
+  'Attire',
+  'Camera',
+  'SmartPhones',
+]
 
 const Products = ({ match }) => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  const alert = useAlert();
+  const alert = useAlert()
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [price, setPrice] = useState([0, 25000]);
-  const [category, setCategory] = useState("");
+  const [currentPage, setCurrentPage] = useState(1)
+  const [price, setPrice] = useState([0, 250000])
+  const [category, setCategory] = useState('')
 
-  const [ratings, setRatings] = useState(0);
+  const [ratings, setRatings] = useState(0)
 
   const {
     products,
@@ -38,27 +38,27 @@ const Products = ({ match }) => {
     productsCount,
     resultPerPage,
     filteredProductsCount,
-  } = useSelector((state) => state.products);
+  } = useSelector((state) => state.products)
 
-  const keyword = match.params.keyword;
+  const keyword = match.params.keyword
 
   const setCurrentPageNo = (e) => {
-    setCurrentPage(e);
-  };
+    setCurrentPage(e)
+  }
 
   const priceHandler = (event, newPrice) => {
-    setPrice(newPrice);
-  };
-  let count = filteredProductsCount;
+    setPrice(newPrice)
+  }
+  let count = filteredProductsCount
 
   useEffect(() => {
     if (error) {
-      alert.error(error);
-      dispatch(clearErrors());
+      alert.error(error)
+      dispatch(clearErrors())
     }
 
-    dispatch(getProduct(keyword, currentPage, price, category, ratings));
-  }, [dispatch, keyword, currentPage, price, category, ratings, alert, error]);
+    dispatch(getProduct(keyword, currentPage, price, category, ratings))
+  }, [dispatch, keyword, currentPage, price, category, ratings, alert, error])
 
   return (
     <Fragment>
@@ -66,32 +66,32 @@ const Products = ({ match }) => {
         <Loader />
       ) : (
         <Fragment>
-          <MetaData title="PRODUCTS -- ECOMMERCE" />
-          <h2 className="productsHeading">Products</h2>
+          <MetaData title='PRODUCTS -- ECOMMERCE' />
+          <h2 className='productsHeading'>Products</h2>
 
-          <div className="products">
+          <div className='products'>
             {products &&
               products.map((product) => (
                 <ProductCard key={product._id} product={product} />
               ))}
           </div>
 
-          <div className="filterBox">
+          <div className='filterBox'>
             <Typography>Price</Typography>
             <Slider
               value={price}
               onChange={priceHandler}
-              valueLabelDisplay="auto"
-              aria-labelledby="range-slider"
+              valueLabelDisplay='auto'
+              aria-labelledby='range-slider'
               min={0}
-              max={25000}
+              max={250000}
             />
 
             <Typography>Categories</Typography>
-            <ul className="categoryBox">
+            <ul className='categoryBox'>
               {categories.map((category) => (
                 <li
-                  className="category-link"
+                  className='category-link'
                   key={category}
                   onClick={() => setCategory(category)}
                 >
@@ -101,41 +101,41 @@ const Products = ({ match }) => {
             </ul>
 
             <fieldset>
-              <Typography component="legend">Ratings Above</Typography>
+              <Typography component='legend'>Ratings Above</Typography>
               <Slider
                 value={ratings}
                 onChange={(e, newRating) => {
-                  setRatings(newRating);
+                  setRatings(newRating)
                 }}
-                aria-labelledby="continuous-slider"
-                valueLabelDisplay="auto"
+                aria-labelledby='continuous-slider'
+                valueLabelDisplay='auto'
                 min={0}
                 max={5}
               />
             </fieldset>
           </div>
           {resultPerPage < count && (
-            <div className="paginationBox">
+            <div className='paginationBox'>
               <Pagination
                 activePage={currentPage}
                 itemsCountPerPage={resultPerPage}
                 totalItemsCount={productsCount}
                 onChange={setCurrentPageNo}
-                nextPageText="Next"
-                prevPageText="Prev"
-                firstPageText="1st"
-                lastPageText="Last"
-                itemClass="page-item"
-                linkClass="page-link"
-                activeClass="pageItemActive"
-                activeLinkClass="pageLinkActive"
+                nextPageText='Next'
+                prevPageText='Prev'
+                firstPageText='1st'
+                lastPageText='Last'
+                itemClass='page-item'
+                linkClass='page-link'
+                activeClass='pageItemActive'
+                activeLinkClass='pageLinkActive'
               />
             </div>
           )}
         </Fragment>
       )}
     </Fragment>
-  );
-};
+  )
+}
 
-export default Products;
+export default Products
